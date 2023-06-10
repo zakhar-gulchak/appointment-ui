0e62b14359635d50f036cc6885edda16a0834a65
Zakhar Gulchak <gulchak.zahar@gmail.com>
2023-06-10 12:13:16 +0200

    chore: hide keys

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp, FirebaseOptions } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCZVP1gtIT5h9iwBD56hgH663vNWHGa7x8',
-  authDomain: 'appointment-service-5c0e0.firebaseapp.com',
-  projectId: 'appointment-service-5c0e0',
-  storageBucket: 'appointment-service-5c0e0.appspot.com',
-  messagingSenderId: '709123247010',
-  appId: '1:709123247010:web:027ce9c4cc6c9b658140fb',
-  measurementId: 'G-MBVFH0YX6Y',
+const firebaseConfig: FirebaseOptions = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY ?? '',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ?? '',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ?? '',
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ?? '',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID ?? '',
+  appId: import.meta.env.VITE_FIREBASE_APP_ID ?? '',
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ?? '',
 }
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
